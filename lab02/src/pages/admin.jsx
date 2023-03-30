9a91df6054fc25a7574e33848a94dc9fef7ac9cb
Walter <walter@Dev2.xsmgcfluo3ke5cbadtolfbtzzc.bx.internal.cloudapp.net>
2023-03-30 03:17:33 +0000

    update

--- lab02/src/pages/admin.jsx
+++ lab02/src/pages/admin.jsx
@@ -7,44 +7,14 @@ import { useSortBy, useTable } from 'react-table';
 import styled from 'styled-components';
 import { useAuth } from '../context/Session';
 import { useFetch } from '../hooks/Fetch';
-
-const Styles = styled.div`
-	padding: 1rem;
-
-	table {
-		thead {
-			background-color: #7c9eae70;
-		}
-
-		border-spacing: 0;
-
-		tr {
-			:last-child {
-				td {
-					border-bottom: 0;
-				}
-			}
-
-			cursor: pointer;
-		}
-
-		th,
-		td {
-			margin: 0;
-			padding: 0.5rem;
-			border-bottom: 1px solid #00000017;
-
-			:last-child {
-				border-right: 0;
-			}
-		}
-	}
-`;
+import DataTable from '../components/dataTable';
 
 export default function Page() {
 	const navigate = useNavigate();
 	const { session } = useAuth();
 	const { data, isLoading, isError, runFetch } = useFetch();
+
+	const rowClick = (row) => console.log('Hola');
 
 	const columns = useMemo(
 		() => [
@@ -73,15 +43,6 @@ export default function Page() {
 		[]
 	);
 
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-		useTable(
-			{
-				columns,
-				data,
-			},
-			useSortBy
-		);
-
 	useEffect(() => {
 		runFetch(`http://20.228.195.178:3001/post/readAll`);
 	}, []);
@@ -89,41 +50,7 @@ export default function Page() {
 	return (
 		<>
 			{session?.state ? (
-				<Styles>
-					<table {...getTableProps}>
-						<thead>
-							{headerGroups.map((headerGroup) => (
-								<tr {...headerGroup.getHeaderGroupProps()}>
-									{headerGroup.headers.map((column) => (
-										<th
-											{...column.getHeaderProps(column.getSortByToggleProps())}
-										>
-											{column.render('Header')}
-										</th>
-									))}
-								</tr>
-							))}
-						</thead>
-						<tbody {...getTableBodyProps()}>
-							{rows.map((row) => {
-								prepareRow(row);
-
-								return (
-									<tr
-										{...row.getRowProps()}
-										onClick={() => sendRowClick(row.original._id)}
-									>
-										{row.cells.map((cell) => {
-											return (
-												<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-											);
-										})}
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				</Styles>
+				<DataTable columns={columns} data={data} rowClick={rowClick} />
 			) : (
 				<Card className="m-auto" style={{ width: '20rem' }}>
 					<Card.Body>
