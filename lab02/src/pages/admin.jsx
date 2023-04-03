fcc983005776f82089f34a569659650441f14d6a
Walter <walter@Dev2.xsmgcfluo3ke5cbadtolfbtzzc.bx.internal.cloudapp.net>
2023-04-03 23:55:15 +0000

    update

--- lab02/src/pages/admin.jsx
+++ lab02/src/pages/admin.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import { useNavigate } from 'react-router-dom';
-import { useSortBy, useTable } from 'react-table';
-import styled from 'styled-components';
 import { useAuth } from '../context/Session';
 import { useFetch } from '../hooks/Fetch';
 import DataTable from '../components/dataTable';
@@ -12,7 +10,9 @@ import DataTable from '../components/dataTable';
 export default function Page() {
 	const navigate = useNavigate();
 	const { session } = useAuth();
+	const [searchValue, setSearchValue] = useState('');
 	const { data, isLoading, isError, runFetch } = useFetch();
+	const [datafiltered, setDataFiltered] = useState([]);
 
 	const rowClick = (row) => console.log('Hola', row);
 
@@ -47,10 +47,31 @@ export default function Page() {
 		runFetch(`http://20.228.195.178:3001/post/readAll`);
 	}, []);
 
+	useEffect(() => {
+		if (searchValue.length >= 3) {
+			return setDataFiltered(
+				data.filter((i) => i.title.toLowerCase() === searchValue.toLowerCase())
+			);
+		}
+		setDataFiltered(data);
+	}, [searchValue, data]);
+
 	return (
 		<>
 			{session?.state ? (
-				<DataTable columns={columns} data={data} rowClick={rowClick} />
+				<>
+					<input
+						type="text"
+						id="search"
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+					></input>
+					<DataTable
+						columns={columns}
+						data={datafiltered}
+						rowClick={rowClick}
+					/>
+				</>
 			) : (
 				<Card className="m-auto" style={{ width: '20rem' }}>
 					<Card.Body>
